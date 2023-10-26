0ef07689959191abec59f0e60d7bd076d5b6428b
Datwarrior <43867639+Datwarrior@users.noreply.github.com>
2023-10-26 18:40:00 +0200

    init

---
+++ assets/script.js
@@ -0,0 +1,67 @@
+const slides = [
+    {
+      image: "slide1.jpg",
+      tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+      image: "slide2.jpg",
+      tagLine:
+        "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+      image: "slide3.jpg",
+      tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+      image: "slide4.png",
+      tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
+  ];
+  
+  const banner = document.getElementById("banner");
+  const bannerImg = banner.querySelector(".banner-img");
+  const bannerText = banner.querySelector("p");
+  const dots = banner.querySelector(".dots");
+  const arrowLeft = banner.querySelector(".arrow_left");
+  const arrowRight = banner.querySelector(".arrow_right");
+  
+  let slideIndex = 0;
+  const slideCount = slides.length;
+  
+  function showNextSlide() {
+    slideIndex = (slideIndex + 1) % slideCount;
+    showSlide();
+  }
+  
+  function showPrevSlide() {
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    showSlide();
+  }
+  
+  function showSlide() {
+    const slide = slides[slideIndex];
+    bannerImg.src = `./assets/images/slideshow/${slide.image}`;
+    bannerText.innerHTML = slide.tagLine;
+  
+    dots.innerHTML = "";
+    for (let i = 0; i < slideCount; i++) {
+      const dot = document.createElement("span");
+      dot.classList.add("dot");
+      if (i === slideIndex) {
+        dot.classList.add("dot_selected");
+      }
+      dots.appendChild(dot);
+    }
+  }
+  
+  showSlide();
+  
+  arrowLeft.addEventListener("click", showPrevSlide);
+  arrowRight.addEventListener("click", showNextSlide);
+  dots.addEventListener("click", (event) => {
+    if (event.target.classList.contains("dot")) {
+      const dotIndex = Array.from(dots.children).indexOf(event.target);
+      slideIndex = dotIndex;
+      showSlide();
+    }
+  });
